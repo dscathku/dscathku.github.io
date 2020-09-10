af31269871a9a4f35884a4c9df2e6079c1cc8c0a
seanchok <seanchok@gmail.com>
2020-09-10 22:02:46 +0800

    build theme demo index page in App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Text } from '@chakra-ui/core';
+import { Box, Stack, Text } from '@chakra-ui/core';
 
 export const App = () => {
-  return <Text color={'brand-blue.500'}>hello world</Text>;
+  return (
+    <Stack spacing={4}>
+      <Text fontSize={'3xl'}>hello world</Text>
+
+      <Box>
+        {['blue', 'red', 'yellow', 'green'].map((color, index) => (
+          <Box key={index} w={'100%'} h={10} backgroundColor={`brand-${color}.400`} />
+        ))}
+      </Box>
+    </Stack>
+  );
 };
