998827436595f7a6d0e4e9be61643d0201b87d24
seanchok <seanchok@gmail.com>
2020-09-19 21:53:32 +0800

    fix theme brand colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Box, Stack, Text } from '@chakra-ui/core';
+import Flex from '@chakra-ui/core/dist/Flex';
 
 export const App = () => {
   return (
     <Stack spacing={4}>
       <Text fontSize={'3xl'}>hello world</Text>
 
-      <Box>
+      <Flex mx={10} justifyContent={'space-between'}>
         {['blue', 'red', 'yellow', 'green'].map((color, index) => (
-          <Box key={index} w={'100%'} h={10} backgroundColor={`brand-${color}.400`} />
+          <Box key={index} w={'20%'} h={40} backgroundColor={`brand-${color}.400`} />
         ))}
-      </Box>
-
-      <Box>hello world again</Box>
+      </Flex>
     </Stack>
   );
 };
