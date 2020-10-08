9cb62cae75d404151501f863e28ca29f68d41225
Jasper <shanhuang88846@gmail.com>
2020-10-08 23:26:19 +0800

    Fixed space-between issue

--- src/components/home/objective/Objective.js
+++ src/components/home/objective/Objective.js
@@ -7,78 +7,142 @@ export const Objective = () => {
     <Box>
       <Box m={8} color={'gray.600'}>
         <Text fontSize="3xl">What do we do?</Text>
-        <Box
-          p={4}
-          display={{ xs: 'block', lg: 'flex' }}
-          bg="gray.100"
-          justify={{ sm: 'space-around', md: 'space-between' }}>
-          <Box px={4} bg="brand-red.50">
-            <Box bg="brand-red.100">
-              <Flex justify={{ lg: 'space-around' }}>
-                <Image p={{ xs: 7 }} src={LogoImage} size="100px"></Image>
-              </Flex>
-              <Text textAlign={{ lg: 'center' }} py={4} fontSize="lg">
-                Develop
-              </Text>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Building applications for various purposes serving a wide audience
-              </Text>
-            </Box>
+        <Box p={4} display={{ lg: 'flex' }} bg="gray.100">
+          <Box width={{ lg: '30vw' }} px={4} bg="brand-red.50">
+            <Flex bg="brand-red.100" display={{ xs: 'flex', lg: 'block' }}>
+              <Image
+                ml={{ lg: 'auto' }}
+                mr={{ lg: 'auto' }}
+                src={LogoImage}
+                width={{ xs: '20vw', lg: '15vw' }}
+                height={{ lg: '15vh' }}></Image>
+
+              <Box px={{ xs: 5, lg: 0 }} bg="brand-red.200">
+                <Text textAlign={{ lg: 'center' }} py={4} fontSize="lg">
+                  Develop
+                </Text>
+
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Building applications for various purposes serving a wide audience
+                </Text>
+              </Box>
+            </Flex>
           </Box>
 
-          <Box px={4} bg="brand-blue.50">
-            <Box bg="brand-blue.100">
-              <Flex justify={{ lg: 'space-around' }}>
-                <Image src={LogoImage} size="100px"></Image>
-              </Flex>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Connect
-              </Text>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Enabling meaningful conversations in a diverse environment
-              </Text>
-            </Box>
+          <Box width={{ lg: '30vw' }} px={4} bg="brand-blue.50">
+            <Flex
+              bg="brand-blue.100"
+              display={{ xs: 'flex', lg: 'block' }}
+              justify="space-between"
+              direction="row-reverse">
+              <Image
+                ml={{ lg: 'auto' }}
+                mr={{ lg: 'auto' }}
+                src={LogoImage}
+                width={{ xs: '20vw', lg: '15vw' }}
+                height={{ lg: '15vh' }}></Image>
+
+              <Box px={{ xs: 5, lg: 0 }} bg="brand-blue.200">
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Connect
+                </Text>
+
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Enabling meaningful conversations in a diverse environment
+                </Text>
+              </Box>
+            </Flex>
           </Box>
 
-          <Box px={4} bg="brand-green.50">
-            <Box bg="brand-green.100">
-              <Flex justify={{ lg: 'space-around' }}>
-                <Image src={LogoImage} size="100px"></Image>
-              </Flex>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Teach
-              </Text>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Deepening our understanding of technology by explaining ideas to peers
-              </Text>
-            </Box>
+          <Box width={{ lg: '30vw' }} px={4} bg="brand-green.50">
+            <Flex bg="brand-green.100" display={{ xs: 'flex', lg: 'block' }}>
+              <Image
+                ml={{ lg: 'auto' }}
+                mr={{ lg: 'auto' }}
+                src={LogoImage}
+                width={{ xs: '20vw', lg: '15vw' }}
+                height={{ lg: '15vh' }}></Image>
+
+              <Box px={{ xs: 5, lg: 0 }} bg="brand-green.200">
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Teach
+                </Text>
+
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Deepening our understanding of technology by explaining ideas to peers
+                </Text>
+              </Box>
+            </Flex>
           </Box>
 
-          <Box px={4} bg="brand-yellow.50">
-            <Box bg="brand-yellow.100">
-              <Flex justify={{ lg: 'space-around' }}>
-                <Image src={LogoImage} size="100px"></Image>
-              </Flex>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Design
-              </Text>
-              <Text textAlign={{ lg: 'center' }} py={4}>
-                Guiding every part of our work from marketing to tech through "design with purpose"
-              </Text>
-            </Box>
+          <Box width={{ lg: '30vw' }} px={4} bg="brand-yellow.50">
+            <Flex
+              bg="brand-yellow.100"
+              display={{ xs: 'flex', lg: 'block' }}
+              justify="space-between"
+              direction="row-reverse">
+              <Image
+                ml={{ lg: 'auto' }}
+                mr={{ lg: 'auto' }}
+                src={LogoImage}
+                width={{ xs: '20vw', lg: '15vw' }}
+                height={{ lg: '15vh' }}></Image>
+
+              <Box px={{ xs: 5, lg: 0 }} bg="brand-yellow.200">
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Design
+                </Text>
+
+                <Text textAlign={{ lg: 'center' }} py={4}>
+                  Guiding every part of our work from marketing to tech through "design with
+                  purpose"
+                </Text>
+              </Box>
+            </Flex>
           </Box>
         </Box>
       </Box>
 
       {/* Testing out responsive design */}
-      <Box bg="gray.100" display={['flex', 'block', 'flex', 'block']}>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
+      <Box px={4} bg="brand-yellow.50">
+        <Flex bg="brand-yellow.100" justify="space-between">
+          <Image bg="gray.300" src={LogoImage} size="100px"></Image>
+          <Image bg="gray.300" src={LogoImage} size="100px"></Image>
+
+          <Box>
+            <Text>I'm here to screw you up!</Text>
+            <Text>Now you know!</Text>
+          </Box>
+        </Flex>
       </Box>
+
+      {/* <Box px={4} bg="brand-yellow.50">
+        <Flex bg="brand-yellow.100" justify = "space-around">
+          <Flex bg = "brand-yellow.200">
+            
+            <Image bg = "gray.300" src = {LogoImage} size="100px"></Image>
+            
+            <Box px = {{xs: 5, lg: 0}}>
+              <Text py = {4}>
+                Design
+              </Text>
+              <Text py = {4}>
+                Guiding every part of our work from marketing to tech through "design with purpose"
+              </Text>
+            </Box>
+
+          </Flex>
+        </Flex>
+      </Box> */}
+
+      <Flex bg="gray.100" justify="space-between">
+        <Text>Trying out chakra-ui responsive design</Text>
+        <Text>Trying out chakra-ui responsive design</Text>
+        <Text>Trying out chakra-ui responsive design</Text>
+        <Text>Trying out chakra-ui responsive design</Text>
+        <Text>Trying out chakra-ui responsive design</Text>
+        <Text>Trying out chakra-ui responsive design</Text>
+      </Flex>
     </Box>
   );
 };
