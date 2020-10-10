ea9f319fb2085352256b3c7e655a5a4219f36488
Jasper <shanhuang88846@gmail.com>
2020-10-10 12:11:30 +0800

    Finalized Design #1 -Added box shadows, rounded icons

--- src/components/home/objective/Objective.js
+++ src/components/home/objective/Objective.js
@@ -1,150 +1,116 @@
 import React from 'react';
 import { Box, Text, Flex, Image } from '@chakra-ui/core';
-import { LogoImage, DesignImage, DevelopImage, ConnectImage, TeachImage } from '../../../assets';
+import { DesignImage, DevelopImage, ConnectImage, TeachImage } from '../../../assets';
 
 export const Objective = () => {
   return (
-    <Box>
-      <Box m={8} color={'gray.600'}>
-        <Text fontSize="3xl">What do we do?</Text>
-        <Box p={4} display={{ lg: 'flex' }}>
-          <Box width={{ lg: '30vw' }} px={4}>
-            <Flex display={{ xs: 'flex', lg: 'block' }}>
-              <Image
-                bg="brand-red.400"
-                mx={{ lg: 'auto' }}
-                my={{ xs: ' auto' }}
-                src={DevelopImage}
-                size={{ xs: '12vh', lg: '15vh' }}></Image>
+    <Box m={8} color={'gray.500'}>
+      <Text fontSize={{ sm: '2xl', md: '3xl', lg: '4xl' }}>What do we do?</Text>
+      <Box pt={4} display={{ lg: 'flex' }}>
+        <Box width={{ lg: '30vw' }}>
+          <Flex bg={{ xs: 'brand-red.50', lg: 'white' }} display={{ xs: 'flex', lg: 'block' }}>
+            <Image
+              boxShadow={{ lg: '4px 4px 6px 0px' }}
+              bg="brand-red.400"
+              mx={{ lg: 'auto' }}
+              my={{ xs: ' auto' }}
+              p={2}
+              src={DevelopImage}
+              size={{ xs: '12vh', lg: '10vw', xl: '150px' }}
+              borderRadius="5%"></Image>
 
-              <Box px={{ xs: 5, lg: 0 }}>
-                <Text textAlign={{ lg: 'center' }} py={4} fontSize="lg">
-                  Develop
-                </Text>
+            <Box mx={{ lg: '5vw' }} px={{ xs: '4', lg: '0' }}>
+              <Text fontSize={{ xs: 'md', md: 'xl' }} textAlign={{ lg: 'center' }} py={3}>
+                Develop
+              </Text>
 
-                <Text textAlign={{ lg: 'center' }} py={4}>
-                  Building applications for various purposes serving a wide audience
-                </Text>
-              </Box>
-            </Flex>
-          </Box>
-
-          <Box width={{ lg: '30vw' }} px={4}>
-            <Flex
-              display={{ xs: 'flex', lg: 'block' }}
-              justify="space-between"
-              direction="row-reverse">
-              <Image
-                bg="brand-blue.400"
-                mx={{ lg: 'auto' }}
-                my={{ xs: ' auto' }}
-                src={ConnectImage}
-                size={{ xs: '12vh', lg: '15vh' }}></Image>
-
-              <Box px={{ xs: 5, lg: 0 }}>
-                <Text textAlign={{ lg: 'center' }} py={4}>
-                  Connect
-                </Text>
-
-                <Text textAlign={{ lg: 'center' }} py={4}>
-                  Enabling meaningful conversations in a diverse environment
-                </Text>
-              </Box>
-            </Flex>
-          </Box>
-
-          <Box width={{ lg: '30vw' }} px={4}>
-            <Flex display={{ xs: 'flex', lg: 'block' }}>
-              <Image
-                bg="brand-green.400"
-                mx={{ lg: 'auto' }}
-                my={{ xs: ' auto' }}
-                src={TeachImage}
-                size={{ xs: '12vh', lg: '15vh' }}></Image>
-
-              <Box px={{ xs: 5, lg: 0 }}>
-                <Text textAlign={{ lg: 'center' }} py={4}>
-                  Teach
-                </Text>
-
-                <Text textAlign={{ lg: 'center' }} py={4}>
-                  Deepening our understanding of technology by explaining ideas to peers
-                </Text>
-              </Box>
-            </Flex>
-          </Box>
-
-          <Box width={{ lg: '30vw' }} px={4}>
-            <Flex
-              bg="brand-yellow.50"
-              display={{ xs: 'flex', lg: 'block' }}
-              justify="space-between"
-              direction="row-reverse">
-              <Image
-                bg="brand-yellow.400"
-                mx={{ lg: 'auto' }}
-                my={{ xs: ' auto' }}
-                src={DesignImage}
-                size={{ xs: '12vh', lg: '10vw' }}></Image>
-
-              <Box mx={{ lg: '5vw' }} px={{ xs: '4', lg: '0' }} bg="brand-yellow.100">
-                <Text
-                  fontSize={{ xs: 'md', md: 'lg', lg: 'xl' }}
-                  textAlign={{ lg: 'center' }}
-                  py={3}>
-                  Design
-                </Text>
-
-                <Text fontSize={{ xs: 'sm', lg: 'md' }} textAlign={{ lg: 'center' }} pb={2}>
-                  Guiding every part of our work from marketing to tech through "design with
-                  purpose"
-                </Text>
-              </Box>
-            </Flex>
-          </Box>
+              <Text fontSize={{ xs: 'sm', md: 'md' }} textAlign={{ lg: 'center' }} pb={2}>
+                Building applications for various purposes serving a wide audience
+              </Text>
+            </Box>
+          </Flex>
         </Box>
-      </Box>
 
-      {/* Testing out responsive design */}
-      <Box px={4} bg="brand-yellow.50">
-        <Flex bg="brand-yellow.100" justify="space-between">
-          <Image bg="gray.300" src={LogoImage} size="100px"></Image>
-          <Image bg="gray.300" src={LogoImage} size="100px"></Image>
+        <Box width={{ lg: '30vw' }}>
+          <Flex
+            bg={{ xs: 'brand-blue.50', lg: 'white' }}
+            display={{ xs: 'flex', lg: 'block' }}
+            justify="space-between"
+            direction="row-reverse">
+            <Image
+              boxShadow={{ lg: '4px 4px 6px 0px' }}
+              bg="brand-blue.400"
+              mx={{ lg: 'auto' }}
+              my={{ xs: ' auto' }}
+              p={2}
+              src={ConnectImage}
+              size={{ xs: '12vh', lg: '10vw', xl: '150px' }}
+              borderRadius="5%"></Image>
 
-          <Box>
-            <Text>I'm here to screw you up!</Text>
-            <Text>Now you know!</Text>
-          </Box>
-        </Flex>
-      </Box>
+            <Box mx={{ lg: '5vw' }} px={{ xs: '4', lg: '0' }}>
+              <Text fontSize={{ xs: 'md', md: 'xl' }} textAlign={{ lg: 'center' }} py={3}>
+                Connect
+              </Text>
 
-      {/* <Box px={4} bg="brand-yellow.50">
-        <Flex bg="brand-yellow.100" justify = "space-around">
-          <Flex bg = "brand-yellow.200">
-            
-            <Image bg = "gray.300" src = {LogoImage} size="100px"></Image>
-            
-            <Box px = {{xs: 5, lg: 0}}>
-              <Text py = {4}>
+              <Text fontSize={{ xs: 'sm', md: 'md' }} textAlign={{ lg: 'center' }} pb={2}>
+                Enabling meaningful conversations in a diverse environment
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+
+        <Box width={{ lg: '30vw' }}>
+          <Flex bg={{ xs: 'brand-green.50', lg: 'white' }} display={{ xs: 'flex', lg: 'block' }}>
+            <Image
+              boxShadow={{ lg: '4px 4px 6px 0px' }}
+              bg="brand-green.400"
+              mx={{ lg: 'auto' }}
+              my={{ xs: ' auto' }}
+              p={2}
+              src={TeachImage}
+              size={{ xs: '12vh', lg: '10vw', xl: '150px' }}
+              borderRadius="5%"></Image>
+
+            <Box mx={{ lg: '5vw' }} px={{ xs: '4', lg: '0' }}>
+              <Text fontSize={{ xs: 'md', md: 'xl' }} textAlign={{ lg: 'center' }} py={3}>
+                Teach
+              </Text>
+
+              <Text fontSize={{ xs: 'sm', md: 'md' }} textAlign={{ lg: 'center' }} pb={2}>
+                Deepening our understanding of technology by explaining ideas to peers
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+
+        <Box width={{ lg: '30vw' }}>
+          <Flex
+            bg={{ xs: 'brand-yellow.50', lg: 'white' }}
+            display={{ xs: 'flex', lg: 'block' }}
+            justify="space-between"
+            direction="row-reverse">
+            <Image
+              boxShadow={{ lg: '4px 4px 6px 0px' }}
+              bg="brand-yellow.400"
+              mx={{ lg: 'auto' }}
+              my={{ xs: ' auto' }}
+              p={2}
+              src={DesignImage}
+              size={{ xs: '12vh', lg: '10vw', xl: '150px' }}
+              borderRadius="5%"></Image>
+
+            <Box mx={{ lg: '5vw' }} px={{ xs: '4', lg: '0' }}>
+              <Text fontSize={{ xs: 'md', md: 'xl' }} textAlign={{ lg: 'center' }} py={3}>
                 Design
               </Text>
-              <Text py = {4}>
+
+              <Text fontSize={{ xs: 'sm', md: 'md' }} textAlign={{ lg: 'center' }} pb={2}>
                 Guiding every part of our work from marketing to tech through "design with purpose"
               </Text>
             </Box>
-
           </Flex>
-        </Flex>
-      </Box> */}
-
-      <Flex bg="gray.100" justify="space-between">
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-        <Text>Trying out chakra-ui responsive design</Text>
-      </Flex>
+        </Box>
+      </Box>
     </Box>
   );
 };
